0d2a0b9f432150493f48814bae7fe43f3aaf6c4e
kartik-neo <143995170+kartik-neo@users.noreply.github.com>
2024-01-25 10:06:25 +0530

    Update sonarqubeAnalysis.groovy

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -1,5 +1,5 @@
 def call() {
-    withSonarQubeEnv('sonar-server') {
-        sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=youtube -Dsonar.projectKey=youtube '''
+    withSonarQubeEnv('sonar-token') {
+        sh ''' $SCANNER_HOME/bin/sonar-token -Dsonar.projectName=youtube -Dsonar.projectKey=youtube '''
     }
 }
